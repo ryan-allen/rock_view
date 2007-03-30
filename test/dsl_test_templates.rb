f3ea76fa1943a1e62f47558cd4f18b8e9172bf15
Ryan Allen <ryan@yeahnah.org>
2007-03-30 01:41:07 +0000

    forgot this from last time, no we're at where we were with the broken tests...
    git-svn-id: svn://yeahnah.org/rock_view@27 ae97f4e8-f72a-0410-89b5-ac65449ed120

--- test/dsl_test_templates.rb
+++ test/dsl_test_templates.rb
@@ -1,20 +1,25 @@
-module Site
-  class Layout < CView::Template
-    assigns :domain
-    template "<html><%= render 'heading', :heading => domain.upcase %><%= render_sub_templates %></html>"
+CView::Template.create 'site/layout' do
+  def reformat_heading
+    domain.upcase
   end
-  class Heading < CView::Template
-    assigns :heading
-    template '<h1><%= heading %></html>'
-  end
-  class Sidebar < CView::Template
-    template '<div id="sidebar"><%= render_sub_templates %></div>'
-  end
-  class Page < CView::Template
-    template '<div id="content">Welcome to <%= domain %>!</div><% render \'sidebar\' do %>I AM SIDEBAR!<% end %>'
-  end
-  class Footer < CView::Template
-    assigns :contact
-    template '<div id="footer"><%= contact %></div>'
-  end
+  assigns :domain
+  template "<html><%= render 'heading', :heading => domain.upcase %><%= render_sub_templates %></html>"
+end
+
+CView::Template.create 'site/heading' do
+  assigns :heading
+  template '<h1><%= heading %></html>'
+end
+
+CView::Template.create 'site/sidebar' do
+  template '<div id="sidebar"><%= render_sub_templates %></div>'
+end
+
+CView::Template.create 'site/page' do
+  template '<div id="content">Welcome to <%= domain %>!</div><% render \'sidebar\' do %>I AM SIDEBAR!<% end %>'
+end
+
+CView::Template.create 'site/footer' do
+  assigns :contact
+  template '<div id="footer"><%= contact %></div>'
 end
